0a88f779c57f23057b45af5588b291e069c54200
T-ahiti <zannatultasnimtahiti@gmail.com>
2024-07-03 00:27:39 +0600

    second commit

---
+++ case.js
@@ -0,0 +1,10 @@
+const userName = 'blackPink'
+const userInput = 'blackPinK'
+console.log(userName.toLowerCase())
+
+if (userName.toLowerCase() === userInput.toLowerCase){
+    console.log('valid user')
+}
+    else{
+        console.log('invalid user')
+    }
